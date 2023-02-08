6b7040234f60bc9fdf9e547fc8eeae40f6a5316f
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-08 17:17:43 +0200

    themas07-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import * as basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js'; //basiclightbox
 
+// Change code below this line
 console.log(galleryItems);
 
+const gallery = document.querySelector('.gallery');
 
-//const result = _.add(2, 3);
-//console.log(result); // 5
+const imagesMurkup = galleryItems.map((
+    { 
+        preview,
+        original,
+        description
+    }
+) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+    </div>
+`; }).join("");
+
+console.log(imagesMurkup);
+gallery.insertAdjacentHTML("afterbegin", imagesMurkup);
+
+
+/*
+https://www.jsdelivr.com/package/npm/basiclightbox?path=dist
+*/
+
+/*import * as basicLightbox from 'basiclightbox'
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+
+instance.show()*/
+
