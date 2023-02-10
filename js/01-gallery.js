cbc91c6da4eca5da76ce44ec00179f82463c36a6
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-10 15:19:25 +0200

    What I do in 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,14 @@
 import { galleryItems } from './gallery-items.js';
-import * as basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js'; //basiclightbox
 
 // Change code below this line
-console.log(galleryItems);
+
+//console.log(galleryItems);
+//import * as basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js'; //basiclightbox
 
 const gallery = document.querySelector('.gallery');
 
-const galleryMurkup = galleryItems.map((
-    { 
-        preview,
-        original,
-        description
-    }
-) => {
+const galleryMurkup = galleryItems
+  .map(({ preview, original, description }) => {
     return `<div class="gallery__item">
     <a class="gallery__link" href="${original}">
         <img
@@ -23,10 +19,37 @@ const galleryMurkup = galleryItems.map((
         />
     </a>
     </div>
-`; }).join("");
+`;
+  })
+  .join('');
 
-console.log(galleryMurkup);
-gallery.insertAdjacentHTML("afterbegin", galleryMurkup);
+//console.log(galleryMurkup);
+gallery.insertAdjacentHTML('afterbegin', galleryMurkup);
+const dataSourceQ = "data-source=".length;
+//console.log(dataSourceQ);
+
+//instance.show();
+ 
+gallery.addEventListener('click', basicLightboxModal);
+
+function basicLightboxModal(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return;
+  //if (!event.target.classList.contains('gallery__image')) return;
+    //let originalSrc = event.target.attributes[2];
+    //originalSrc= originalSrc.slice(12, originalSrc.length);
+    //console.log(originalSrc);
+    const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`);
+    instance.show();
+}
+
 
 
 /*
@@ -40,4 +63,3 @@ const instance = basicLightbox.create(`
 `)
 
 instance.show()*/
-
